f8dadd9b4b8765b6192df4ba2c3adaba9321ad8c
mehmethalis <mehmethaliscicek@gmail.com>
2022-12-21 17:39:56 +0300

    some change

--- server-ieee754/server.py
+++ server-ieee754/server.py
@@ -1,29 +1,51 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request
 
 from ieee754 import IEEE754
 
 app = Flask(__name__)
 
-@app.route("/", methods=[ "GET"])
+
+@app.route("/", methods=["GET"])
 def root():
-    return {"message":"Works"}
+    return {"message": "Works"}
+
 
 @app.route("/ieee754", methods=["POST", "GET"])
 def ieee754():
     if request.method == "POST":
+        number = request.json["number"]
+        precisionType=request.json["precisionType"]
+
+        # force_length,
+        # force_exponent,
+        # force_mantissa,
+        # force_bias,
+        # precision=2
+
+        # half
+        # signle
+        #..
+        #..
+        if(precisionType == "single"):
+
+
+
+
+
+    
 
         def find_bias(exponent):
             return 2 ** (exponent - 1) - 1
 
-        float_number = float(request.json["integer"] + "." + request.json["decimal"])
-
         b = IEEE754(
-            float_number,
-            force_length=int(request.json["length"]),
-            force_exponent=int(request.json["exponent"]),
-            force_mantissa=int(request.json["mantissa"]),
-            force_bias=find_bias(int(request.json["exponent"])),
+            x=number,
+            precision=precision,
+            force_length=force_length,
+            force_exponent=force_exponent,
+            force_mantissa=force_mantissa,
+            force_bias=force_bias,
         )
-        return jsonify(b)
+
+        return {"data": str(b)}
     else:
         return {"message": "Welcome ieee754 API", "Author": "haliscicek.com/en"}
